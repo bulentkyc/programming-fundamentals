fbef6bf2d5a93e7342e86042312185101a3548f0
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2020-11-25 12:31:35 +0100

    Task is done

--- switch_case.js
+++ switch_case.js
@@ -51,3 +51,119 @@ if (finalSalary > 0) {
     console.log(`Regarding to ${role} role, your salary is ${finalSalary} $`);
 }
 
+
+let age = parseInt(prompt('How old are you?'));
+let day = prompt('Which day you wish to check?').toUpperCase();
+let time = parseInt(prompt('What time you wish to check? \n (Please type just hour as a number 24h)'));
+
+if (time >=9) {
+    if (time <= 13) {
+        time = 1
+    } else if (time <= 15){
+        time = 2
+    } else if (time <= 16){
+        time = 3
+    } else {
+        time = 4
+    }
+}
+
+if (age >=0) {
+    if (age <= 3) {
+        age = 1
+    } else if (age <= 6){
+        age = 2
+    } else if (age <= 12){
+        age = 3
+    } else if (age <= 18){
+        age = 4
+    } else {
+        age = 5
+    }
+}
+
+
+
+
+switch (day) {
+    case 'FRIDAY':
+        switch (age) {
+            case 2:
+                if (time == 1) {
+                    alert(`${day} at ${time} o'clok you should be in Kindergarten`);
+                }
+                break;
+
+            case 3:
+                if (time == 1) {
+                    alert(`${day} at ${time} o'clok you should be at School`);
+                }
+                break;
+
+            case 4:
+                if (time == 2) {
+                    alert(`${day} at ${time} o'clok you should be at School`);
+                }
+                break;
+
+            case 5:
+                if (time == 3) {
+                    alert(`${day} at ${time} o'clok you should be at work`);
+                }
+                break;
+                
+            default:
+            alert(`There's nothing planned.
+            But there's Corona!
+            Stay at home.`);
+                break;
+        }
+        break;
+
+    case 'SATURDAY':
+    case 'SUNDAY':
+        alert(`There's nothing planned.
+        But there's Corona!
+        Stay at home.`);
+        break;
+
+    default:
+        switch (age) {
+            case 1:
+                if (time == 1) {
+                    alert(`${day} at ${time} o'clok you should be in Kindergarten`);
+                }
+                break;
+
+            case 2:
+                if (time == 1) {
+                    alert(`${day} at ${time} o'clok you should be in Kindergarten`);
+                }
+                break;
+
+            case 3:
+                if (time == 1) {
+                    alert(`${day} at ${time} o'clok you should be at School`);
+                }
+                break;
+
+            case 4:
+                if (time == 2) {
+                    alert(`${day} at ${time} o'clok you should be at School`);
+                }
+                break;
+
+            case 5:
+                if (time == 3) {
+                    alert(`${day} at ${time} o'clok you should be at work`);
+                }
+                break;
+                
+            default:
+                alert(`There's nothing planned.
+                But there's Corona!
+                Stay at home.`);
+                break;
+        }
+        break;
+}
